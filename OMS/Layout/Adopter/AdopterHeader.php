9710cb1fd2401ceeaef08a588f70fbe3489a479f
Priyam Sarker <123936892+Priiyaam@users.noreply.github.com>
2024-05-25 12:01:10 +0600

    Add files via upload

---
+++ OMS/Layout/Adopter/AdopterHeader.php
@@ -0,0 +1,50 @@
+<?php 
+    session_start();
+    if(!isset($_SESSION["loginUser_Name"])){
+        header("Location: ../../Views/Adopter/Login.php");
+    }
+
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title></title>
+    <style>
+    nav {
+        float: right;
+        margin-top: 10px;
+    }
+
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+    }
+
+    li {
+        display: inline-block;
+        margin-right: 10px;
+    }
+    </style>
+</head>
+
+<body>
+    <header>
+
+        <nav>
+            <ul>
+
+                <li><span></span><a href="../../Views/Adopter/Profile.php"><?php echo $_SESSION["loginUser_Name"];?></a>
+                </li>
+                <li><a href="../../Controller/Adopter/Logout_Handler.php">Logout</a></li>
+            </ul>
+        </nav>
+    </header>
+    <br>
+    <br>
+    <hr>
+</body>
+
+</html>
---
+++ OMS/Layout/Homepage/Footer.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset='utf-8'>
+    <meta name="viewport" content="width=device-width, initial-scale=1" id="wixDesktopViewport" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="generator" content="Wix.com Website Builder" />
+    <style>
+    body {
+        background-color: #202020;
+    }
+
+    footer {
+        background-color: #000000;
+        padding: 3px;
+        text-align: center;
+        /* position: fixed; */
+        bottom: 0;
+        left: 0;
+        right: 0;
+        font-size: 14px;
+        font-family: "Times New Roman", Times, serif;
+        color: #ffffff;
+    }
+    </style>
+</head>
+
+<body>
+
+
+
+    <footer>
+        <p>Safe Haven</p>
+        <p>official.safe.haven@gmail.com</p>
+        <div class="container">
+            <p>&SAFE; <?php echo date('Y'); ?> by safe Haven</p>
+        </div>
+
+    </footer>
+
+
+
+
+
+</body>
+
+</html>
